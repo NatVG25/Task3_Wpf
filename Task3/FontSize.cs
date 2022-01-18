b3ff851e684b2544fa5e336b1ba568579df77823
nvg25 <nvg25@bk.ru>
2022-01-18 23:49:02 +0500

    Добавьте файлы проекта.

---
+++ Task3/FontSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class FontSize
+    {
+        private string fontSize;
+
+        public FontSize(string fontSize)
+        {
+            this.fontSize = fontSize;
+        }
+
+        public FontSize(double fontSizeDouble)
+        {
+        }
+
+        public static implicit operator double(FontSize v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task3_1/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+       
+        private void ComboBox_ChangeFontSize(object sender, SelectionChangedEventArgs e) //обработчик события выбора размера шрифта
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text; //считываем выбранную позицию из списка
+            double fontSizeDouble = Convert.ToDouble(fontSize); //преобразуем string в double
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSizeDouble; //присваиваем размер шрифта в textBox
+            }
+        }
+
+        private void RadioButton_Checked1(object sender, RoutedEventArgs e)//обработчик события выбора цвета шрифта
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black; //обращаемся к textBox и присваиваем ему в свойство Foreground - цвет черный
+            }
+        }
+
+        private void RadioButton_Checked2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void Bold_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+        }
+
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.FontStyle= FontStyles.Italic;
+            }
+        }
+
+        private void Underline_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+        }
+
+        
+
+
+        //TextDecortion - подчеркнутый шрифт - Underline
+        //FontSize - размер шрифта 
+        //FontWeight - толщина шрифта - Black
+        //FontStyle - наклон шрифта = Italic
+    }
+}
+
+
+
